6e49f55ab46a1bf6a4babe0676e3c7e1b7626a17
Mike Frazier <michael.s.frazier@gmail.com>
2016-02-11 13:20:12 -0600

    Updated Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
-      options: {
-        separator: ';'
-      },
       dist: {
         src: ['public/client/*.js'],
         dest: 'public/dist/client.js'
